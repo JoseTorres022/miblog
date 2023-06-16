c0dc395323de75a42a679baafd2e4baf9dc81ed7
Jose Torres <joseangel.tosa@outlook.com>
2023-06-16 14:56:23 -0600

    Relacion entre modelos

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,25 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    //Relacion uno a michos inversa
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class);
+    }
+
+    //Relacion uno a uno. polimorfica
+    public function image(){
+        return $this->morphOne(Image::class, 'imageable');
+    }
 }
--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Tag extends Model
 {
     use HasFactory;
+
+    //Relacion muchos  amuchos
+    public function posts(){
+        return $this->belongsToMany(Post::class);
+    }
 }
